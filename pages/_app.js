a8f743a10ce7fc5a4c6d2ce41d2520f4cb0fb4ce
rashidmalik107 <rashidmalik107@gmail.com>
2021-06-26 19:12:13 +0500

    styling changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,4 @@
 import '../styles/globals.css';
-import '../styles/buttons.css';
-import '../styles/BlockBoxes.css';
-import '../styles/BlockCards.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
