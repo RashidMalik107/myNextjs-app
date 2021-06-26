118aae91d8a9f4b08ec30444105816fc0bfdd0b2
rashidmalik107 <rashidmalik107@gmail.com>
2021-06-26 19:34:46 +0500

    relative path image

--- components/BlockBoxes/data.js
+++ components/BlockBoxes/data.js
@@ -3,49 +3,49 @@ export const blocksData = [
         name: 'Cody Fisher',
         designation: 'Front end web dev',
         role: 'Admin',
-        url: 'https://8degreethemes.com/demo/edict-html/images/t1.jpg'
+        url: '/john.jpg'
     },
     {
         name: 'Cody Fisher',
         designation: 'Front end web dev',
         role: 'Admin',
-        url: 'https://8degreethemes.com/demo/edict-html/images/t1.jpg'
+        url: '/john.jpg'
     },
     {
         name: 'Cody Fisher',
         designation: 'Front end web dev',
         role: 'Admin',
-        url: 'https://8degreethemes.com/demo/edict-html/images/t1.jpg'
+        url: '/john.jpg'
     },
     {
         name: 'Cody Fisher',
         designation: 'Front end web dev',
         role: 'Admin',
-        url: 'https://8degreethemes.com/demo/edict-html/images/t1.jpg'
+        url: '/john.jpg'
     },
     {
         name: 'Cody Fisher',
         designation: 'Front end web dev',
         role: 'Admin',
-        url: 'https://8degreethemes.com/demo/edict-html/images/t1.jpg'
+        url: '/john.jpg'
     },
     {
         name: 'Cody Fisher',
         designation: 'Front end web dev',
         role: 'Admin',
-        url: 'https://8degreethemes.com/demo/edict-html/images/t1.jpg'
+        url: '/john.jpg'
     },
     {
         name: 'Cody Fisher',
         designation: 'Front end web dev',
         role: 'Admin',
-        url: 'https://8degreethemes.com/demo/edict-html/images/t1.jpg'
+        url: '/john.jpg'
     },
     {
         name: 'Cody Fisher',
         designation: 'Front end web dev',
         role: 'Admin',
-        url: 'https://8degreethemes.com/demo/edict-html/images/t1.jpg'
+        url: '/john.jpg'
     },
 
 ];
--- components/BlockCards/data.js
+++ components/BlockCards/data.js
@@ -3,19 +3,19 @@ export const blocksCardData = [
         name: 'Cody Fisher',
         designation: 'Front end web dev',
         role: 'Admin',
-        url: 'https://8degreethemes.com/demo/edict-html/images/t1.jpg'
+        url: '/john.jpg'
     },
     {
         name: 'Cody Fisher',
         designation: 'Front end web dev',
         role: 'Admin',
-        url: 'https://8degreethemes.com/demo/edict-html/images/t1.jpg'
+        url: '/john.jpg'
     },
     {
         name: 'Cody Fisher',
         designation: 'Front end web dev',
         role: 'Admin',
-        url: 'https://8degreethemes.com/demo/edict-html/images/t1.jpg'
+        url: '/john.jpg'
     },
 
 ];
